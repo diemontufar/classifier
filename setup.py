a41a0b78507bea815b03c9a9f8dc0e1d27a72feb
Diego Montúfar <diogonal0@gmail.com>
2015-04-25 23:07:57 +1000

    New update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'tweet_classifier',
   packages = ['tweet_classifier'],
-  version = '1.1.2',
+  version = '1.1.3',
   package_data={'': ['sentiments.json']},
   description = 'A sentiment classifier for tweets',
   author = 'Diego Montufar',
   author_email = 'diogonal0@gmail.com',
   url = 'https://github.com/diogonal/classifier', 
-  download_url = 'https://github.com/diogonal/classifier/tarball/v1.1.2', 
+  download_url = 'https://github.com/diogonal/classifier/tarball/v1.1.3', 
   keywords = ['tweet', 'sentiment', 'classifier'], 
   classifiers=[
         'Environment :: Console',
